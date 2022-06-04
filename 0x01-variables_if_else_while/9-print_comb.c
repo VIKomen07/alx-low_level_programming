358a59df65b41fbf6b082a9432376a680f2abd4e
Victory Omenihu <vicky4zee@gmail.com>
2022-06-04 07:36:46 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+# include <stdio.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		putchar(n + '0');
+		if (n < 9)
+		{
+			putchar(44);
+			putchar(32);
+		}
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
