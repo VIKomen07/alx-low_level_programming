91f5b19ec85ad441e465cb6a7a093a4520a73ae8
Victory Omenihu <vicky4zee@gmail.com>
2022-06-04 07:03:07 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+# include <stdio.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		if (alpha != 'q' && alpha != 'e')
+			putchar(alpha);
+		alpha++;
+	}
+	putchar('\n');
+	return (0);
+}
