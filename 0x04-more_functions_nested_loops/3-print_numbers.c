7bc0fa8f41646478f0e0dc7d03fe2b53fd45ac9e
Victory Omenihu <vicky4zee@gmail.com>
2022-06-10 09:09:12 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - Entry point
+ * description - prints  the numbers, from 0 to 9, followed by a new line
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	char c = 0;
+
+	while (c <= 9)
+	{
+		_putchar('0' + c);
+		c++;
+	}
+	_putchar('\n');
+}
