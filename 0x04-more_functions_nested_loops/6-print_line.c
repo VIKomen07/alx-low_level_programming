63bb72ae0fc7fe41603321181db993c983f26809
Victory Omenihu <vicky4zee@gmail.com>
2022-06-10 09:19:05 -0700

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - Entry point
+ * @c: int type number
+ * Return: void
+ */
+
+void print_line(int c)
+{
+	char d = '_';
+
+	while (c > 0)
+	{
+		_putchar(d);
+		c--;
+	}
+	_putchar('\n');
+}
